9cf9e0a6bd71ba00ebb2285b2b8f6b6003f78bcb
github_Jeptooleonida <leonida.jeptoo@student.moringaschool.comgit config --global user.name Jeptooleonidagit config --global user.email leonida.jeptoo@student.moringaschool.com>
2022-09-16 11:47:20 +0300

    update controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,23 +1,19 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
-  
   # Add your routes here
   get "/" do
     event = Event.all
-    event.to_json(include: { venue: { only: [:venue_name, :capacity, :phone, :address] }, 
+    event.to_json(include: { venue: { only: [:venue_name, :capacity, :phone, :address] },
       user: { only: [:username] }})
   end
-
   get '/user' do
     user = User.first
     user.to_json(include: :event)
   end
-
-  get '/venues' do 
+  get '/venues' do
     venues = Venue.all
     venues.to_json( only: [:venue_name, :id] )
   end
-
   post '/add-venue' do
     venue = Venue.create(
       venue_name: params[:venue_name],
@@ -27,7 +23,6 @@ class ApplicationController < Sinatra::Base
     )
     venue.to_json
   end
-
   post '/add-event' do
     event = Event.create(
       venue_id: params[:venue_id],
@@ -39,35 +34,29 @@ class ApplicationController < Sinatra::Base
     )
     event.to_json
   end
-
   delete '/events/:id' do
     event = Event.find(params[:id])
     event.destroy.to_json
   end
-
-  patch '/events/:id' do 
+  patch '/events/:id' do
     event = Event.find(params[:id])
     event.update(
         event_type: params[:event_type],
         attendees: params[:attendees],
         price: params[:price],
         date: params[:date],
-
     )
-    event.to_json(include: { venue: { only: [:venue_name, :capacity, :phone, :address] }, 
+    event.to_json(include: { venue: { only: [:venue_name, :capacity, :phone, :address] },
       user: { only: [:username] }})
   end
-
   get '/most-popular' do
    user = User.most_popular
    user.to_json
   end
-
   get '/most-skrilla' do
     user = User.popping
     user.to_json
   end
-
   get '/busy-bee' do
     user = User.busy_beaver
     user.to_json
