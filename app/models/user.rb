6ae9b644012b2659b2852052af46e75b9c6ef927
github_Jeptooleonida <leonida.jeptoo@student.moringaschool.comgit config --global user.name Jeptooleonidagit config --global user.email leonida.jeptoo@student.moringaschool.com>
2022-09-19 13:17:41 +0300

    Done

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,33 +3,5 @@ class User < ActiveRecord::Base
     has_many :venues, through: :events
 
     
-    def total_fans
-        events.pluck(:attendees).sum
-    end
-
-    def self.most_popular
-        username = self.all.sort { |a, b| a.total_fans <=> b.total_fans }.last.username
-        fans = self.all.sort { |a, b| a.total_fans <=> b.total_fans }.last.total_fans
-        "Name: #{username} - Number of Fans: #{fans}"
-    end
-
-    def self.highest_grossing_concert
-        eventsArr = self.all.map{ |u| u.events}.map{ |e| e.attendees}
-        eventsArr
-    end
-
-    def earnings
-        self.events.map { |event| event.attendees * event.price }.max
-      end
-    
-      def self.popping
-          username = self.all.sort { |a, b| a.earnings <=> b.earnings }.last.username
-          cashmoney = self.all.sort { |a, b| a.earnings <=> b.earnings }.last.earnings
-          "Name: #{username} - Highest grossing event: $#{cashmoney}"
-      end
-
-      def self.busy_beaver 
-        self.all.max{|u| u.events.length}.username
-      end
-
+   
 end
